04430707463ca74aedb694e2df069e38c3b59881
Oz-Asher <ozasher12@gmail.com>
2025-05-12 12:48:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#main.py: Entry point for the project
+
+import os
+
+from src.mismatches import mismatches
+from src.excel_editing import excel_editing
+from src.statistics import statistics
+from src.search_unit import search
+
+
+# Get the directory of main.py
+directory = os.path.dirname(os.path.abspath(__file__))
+
+
+csv_names = ['Oz', 'Shaked']
+
+
+
+combined = mismatches(csv_names)
+
+excel_editing(combined, directory)
+
+statistics(combined)
+
+search('final')
+
+
+
+
+
+
+
+
+
---
+++ src/csv_class.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import pandas as pd
+
+class CSV:
+
+    def __init__(self, name):
+        self.name = name.strip().lower()
+        self.excel_directory = fr"{os.path.dirname(os.path.abspath(__file__))}\{self.name}.csv"  # Path to the CSV file
+       
+        # Load the CSV file into a DataFrame using ISO-8859-1 encoding to handle special characters
+        self.df = pd.read_csv(self.excel_directory, encoding='ISO-8859-1')  # Reading CSV into pandas DataFrame
+
+    def full_file(self):
+        return self.df
+
+    # Get the list of column names in the DataFrame
+    def columns(self):
+        return self.df.columns.tolist()
+
+    # Get the list of values in a specific column of the DataFrame
+    def rows(self, column):
+        return self.df[column].tolist()
+
+    # Insert a row with NaN values at a specified index
+    def insert_none_at(self, index):
+        new_row = {column: np.nan for column in self.df.columns}  # Create a new row with NaN values
+        self.df = pd.concat([  # Insert the NaN row at the specified index
+            self.df.iloc[:index],
+            pd.DataFrame([new_row]),
+            self.df.iloc[index:]
+        ], ignore_index=True)
+        
+
+
